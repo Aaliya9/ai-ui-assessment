cb56c8f77b8076f3d7ff69a2436a24efc5f83641
Aaliya9 <aaliyasharif1998@gmail.com>
2025-08-25 20:38:13 +0530

    fix: use output export for static site

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  assetPrefix: isProd ? '/ai-ui-assessment/' : '',
-  images: {
-    unoptimized: true,
-  },
+// module.exports = {
+//   assetPrefix: isProd ? '/ai-ui-assessment/' : '',
+//   images: {
+//     unoptimized: true,
+//   },
+//   output: 'export',
+// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
 };
+
+module.exports = nextConfig;
